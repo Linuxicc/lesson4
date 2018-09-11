fb4896e8a38d6910cb09859658bdb5720704a7c9
elenanagornova <linuxic2801@1991lena>
2018-09-11 22:04:06 +0300

    Refactoring

--- src/main/kotlin/ru/yamoney/test/App.kt
+++ src/main/kotlin/ru/yamoney/test/App.kt
@@ -21,10 +21,8 @@ fun main(args: Array<String>) {
             "DEPOSIT" -> billing.addOperation(Deposit(BigDecimal(args[2]), args[1]))
             "BALANCE" -> billing.getUserBalance(args[1])
             "SHOP_INFO" -> billing.getShopIdOperations(args[1])
-            //"P2P" -> {billing.addOperation(Deposit(BigDecimal(args[2]), args[3]))
-              //  billing.addOperation(WithDraw(BigDecimal(args[2]), args[1]))}
             "P2P" -> billing.addOperation(P2P(BigDecimal(args[1]), args[2], args[3]))
-            else -> throw IllegalArgumentException("Unknown Command")
+            else -> throw IllegalArgumentException("Unknown Command") as Throwable
         }
     } catch (e: Throwable) {
         e.printStackTrace()
--- src/test/kotlin/ru/yamoney/test/MyTest.kt
+++ src/test/kotlin/ru/yamoney/test/MyTest.kt
@@ -1,25 +1,70 @@
 package ru.yamoney.test
 
-import org.junit.jupiter.api.AfterEach
+import com.winterbe.expekt.should
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import java.io.File
 
-val SUM=123.toBigDecimal()
-val NAME="Olga"
-val SHOP_ID="85351"
+val SUM = 123.toBigDecimal()
+val USERNAME = "Olga"
+val USERNAME_TO = "Ivan"
+val SHOP_ID = "85351"
+val depositLine = "ru.yamoney.test.Deposit||{\"sum\":%s,\"user\":\"%s\"}"
+val paymentLine = "ru.yamoney.test.Payment||{\"sum\":%s,\"user\":\"%s\",\"shopid\":\"%s\"}"
+val p2pLine = "ru.yamoney.test.P2P||{\"sum\":%s,\"user\":\"%s\",\"userTo\":\"%s\"}"
 
 class MyTest {
     private val file = File("operations")
+    val billing = BillingImpl()
 
     @BeforeEach
-    fun deleteFileBefore()
-    {
-        try{file.delete()}catch(e:Exception){e.printStackTrace()}
+    fun deleteFileBefore() {
+        try {
+            file.delete()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
+    @DisplayName("Проверка записи операции Deposit в файл")
     @Test
-    fun checkStringInFile(){
-        Payment(user = NAME, sum=SUM, shopid = SHOP_ID)
+    fun addOperationDepositTest() {
+        val deposit = Deposit(sum = SUM, user = USERNAME)
+        billing.addOperation(deposit)
+        val strFromFile = file.readLines()
+        assertAll("Проверяем, что в файл записана нужная строка", {
+            strFromFile.count().should.be.equal(1)
+            strFromFile.first().should.contain(depositLine.format(SUM, USERNAME))
+        })
+
     }
+
+    @DisplayName("Проверка записи операции Payment в файл")
+    @Test
+    fun addOperationPaymentTest() {
+        val payment = Payment(sum = SUM, user = USERNAME, shopid = SHOP_ID)
+        billing.addOperation(payment)
+        val strFromFile = file.readLines()
+        assertAll("Проверяем, что в файл записана нужная строка", {
+            strFromFile.count().should.be.equal(1)
+            strFromFile.first().should.contain(paymentLine.format(SUM, USERNAME, SHOP_ID))
+        })
+
+    }
+
+    @DisplayName("Проверка записи операции P2P в файл")
+    @Test
+    fun addOperationP2PTest() {
+        val p2p = P2P(sum = SUM, user = USERNAME, userTo = USERNAME_TO)
+        billing.addOperation(p2p)
+        val strFromFile = file.readLines()
+        assertAll("Проверяем, что в файл записалась нужная строка", {
+            strFromFile.count().should.be.equal(1)
+            strFromFile.first().should.contain(p2pLine.format(SUM, USERNAME, USERNAME_TO))
+        })
+    }
+
+
 }
